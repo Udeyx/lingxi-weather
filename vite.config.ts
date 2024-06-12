75e2e04c0dcb405514a19592ae001e204ceb587d
Udeyx <udeyxx@gmail.com>
2024-06-12 10:59:51 +0800

    do a lot of things

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,26 @@ export default defineConfig({
         VueDevTools(),
         Components({
             resolvers: [PrimeVueResolver()]
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            includeAssets: ['logo.png'],
+            manifest: {
+                name: '灵犀气象——合你心意的气象大数据平台',
+                short_name: '灵犀气象',
+                description: '合你心意的气象大数据平台',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'logo.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    }
+                ]
+            }
         })
     ],
     resolve: {
